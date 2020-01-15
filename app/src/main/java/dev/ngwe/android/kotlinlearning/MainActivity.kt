f21324a3255aea7ef36699858d822b3e88dbe83f
lamin <lamin.ngweyeeshoon@gmail.com>
2020-01-15 16:19:10 +0800

    inheritence, open keyword

--- app/src/main/java/dev/ngwe/android/kotlinlearning/MainActivity.kt
+++ app/src/main/java/dev/ngwe/android/kotlinlearning/MainActivity.kt
@@ -9,17 +9,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val person1 = Person("ngwe yee shoon", 25)
-        println("First Name = ${person1.firstName}")
-        println("Age = ${person1.age}")
-
-        // secondary constructor
-        val HUman = HUman("TutorialsPoint.com", 25)
-        println(
-            HUman.message + HUman.firstName +
-                    "Welcome to the example of Secondary  constructor, Your Age is-${HUman.age}"
-        )
+        var bcd = BCD()
+        bcd.think()
     }
 
 
+}
+
+open class ABC {
+    open fun think() {
+        println("Hey!! I am thinking")
+    }
+}
+
+class BCD : ABC() {
+
+    override fun think() {
+        println("I am thinking from child class")
+    }
 }
