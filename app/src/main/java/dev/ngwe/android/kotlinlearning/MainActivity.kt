08d9b335ab7f7b31ad4860251db528b0d2728568
lamin <lamin.ngweyeeshoon@gmail.com>
2020-01-16 15:40:34 +0800

    Delegation(by, lazy, observable(), Delegate())

--- app/src/main/java/dev/ngwe/android/kotlinlearning/MainActivity.kt
+++ app/src/main/java/dev/ngwe/android/kotlinlearning/MainActivity.kt
@@ -2,6 +2,8 @@ package dev.ngwe.android.kotlinlearning
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,26 +11,64 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val b = BaseImpl(10)
+        Derived(b).printMe()
 
-        var objet = genericExample("JAVA")
-        var objet1 = genericExample(10)
+        println("$myVar My dear friend")
 
-        val object2 = genericExample<Int>(10)
-        val object3 = genericExample<Double>(10.00)
-        println(object2)
-        println(object3)
+        val user = User()
+        user.name = "first"
+        user.name = "second"
+
+
+        val e= Example()
+        println(e.p)
+
+
     }
 
 
 }
-
-class genericExample<T>(input: T) {
-
-    init {
-        println("I am getting called with the value $input")
-
-    }
-
+class Example {
+    var p: String by Delegate()
 
 }
+
+
+interface Base {
+    fun printMe() //abstract method
+}
+
+class BaseImpl(val x: Int) : Base {
+    override fun printMe() {
+        println(x)
+    }   //implementation of the method
+}
+
+class Derived(b: Base) : Base by b
+
+
+val myVar: String by lazy {
+    "Hello"
+}
+
+
+class User {
+
+    var name: String by Delegates.observable("Welcome to Tutorialspoint.com") { property, oldValue, newValue ->
+        println("$oldValue -> $newValue")
+    }
+}
+
+
+class Delegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return "$thisRef, thank you for delegating '${property.name}' to me!"
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        println("$value has been assigned to '${property.name} in $thisRef.'")
+    }
+}
+
 
